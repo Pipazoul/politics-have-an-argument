a0ed1ac0020103c0062cb2ae64157b0925aefced
Default <yassin@siouda.com>
2020-02-24 12:07:19 +0100

    added search conf and defined speakers

--- request_by_user.py
+++ request_by_user.py
@@ -2,21 +2,42 @@
 
 import twitter
 from gtts import gTTS
-from playsound import playsound
 import os
 import re
 from datetime import datetime
 import shutil
 import json 
+import subprocess
+
 
 # Getting configuration params
 with open('config.json') as json_data_file:
     conf = json.load(json_data_file)
 
+# API Credentials
 consumer_key = conf["consumer_key"]
 consumer_secret = conf["consumer_secret"]
 access_token_key = conf["access_token_key"]
 access_token_secret = conf["access_token_secret"]
+
+# Searching params
+generateAudio = True
+createSubFolders = True
+
+searchByMonth = conf["searchByMonth"]
+querySearch =  True
+
+count = conf["tweet_count"]
+tweetPerPolitic = conf["tweet_number"]
+monthDate = conf["monthDate"]
+query = conf["query"]
+
+
+# Speakers identification
+speaker1 = conf["speaker1"]
+speaker2 = conf["speaker2"]
+speaker3 = conf["speaker3"]
+
 
 
 api = twitter.Api(consumer_key= consumer_key,
@@ -78,28 +99,40 @@ def genTTS(i, result, account, language) :
                 tts.save('output/'+account+'/audio'+ str(i) +'.mp3')
             else :
                 tts.save('output/'+account+'_audio'+ str(i) +'.mp3')
+            # Generate the m3u playlist
+            #file = open("playlist.m3u","w") 
+            
+            #file.write("#EXTM3U") 
+            #file.write(“This is our new text file”) 
+            #file.write(“and this is another line.”) 
+            #file.write(“Why? Because we can.”) 
+            
+            #file.close() 
             print(account)
 
-
-
-generateAudio = True
-createSubFolders = True
-
-searchByMonth = False
-querySearch =  True
-
-count = 1000
-tweetPerPolitic = 10
-monthDate = 12
-query = ""
 
 if generateAudio == True :
     shutil.rmtree('output')
     os.mkdir('output')
 
+print("******************TWITTER TO TTS********************")
+print("Numbers of tweet to search : " + str(count))
+print("Tweet nb by user :" + str(tweetPerPolitic))
+print("Search by month :" + str(searchByMonth))
+if searchByMonth == True :
+    print("Month number : " + str(monthDate))
+print("Query : " + query)
+print("****************************************************")
+
+
+getPoliticTweet(speaker1[0], speaker1[1])
+getPoliticTweet(speaker2[0], speaker1[1])
+getPoliticTweet(speaker3[0], speaker1[1])
+
+# python request_by_user.py realDonaldTrump en
 #International politics
-getPoliticTweet("realDonaldTrump", "en")
-getPoliticTweet("borisjohnson", "en")
-getPoliticTweet("barackobama", "en")
-getPoliticTweet("AOC", "en")
-getPoliticTweet("berniesanders", "en")
+#getPoliticTweet("realDonaldTrump", "en")
+#getPoliticTweet("borisjohnson", "en")
+#getPoliticTweet("barackobama", "en")
+#getPoliticTweet("AOC", "en")
+#getPoliticTweet("berniesanders", "en")
